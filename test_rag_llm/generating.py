79fbf7fffebbf1bc4aeed94930bfeb6fb8e9cfc0
Hu Dou <hugh.dou@gmail.com>
2025-06-28 21:05:26 -0400

    test_rag_llm - use rag_chain for generating script

--- test_rag_llm/generating.py
+++ test_rag_llm/generating.py
@@ -2,7 +2,9 @@ from retrieving import get_retriever
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.documents import Document
 
 template = """Answer the question based only on the following context:
 {context}
@@ -12,27 +14,37 @@ Question: {question}
 
 prompt = ChatPromptTemplate.from_template(template)
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-chain = prompt | llm | StrOutputParser()
+# chain = prompt | llm | StrOutputParser()
 
 retriever = get_retriever(n_results=3)
 
-def generate_answer(question:str):
-    docs = retriever.invoke(question)
+def format_context(docs:list[Document]):
     context = ""
     for doc in docs:
         context += doc.page_content
         for k, v in doc.metadata.items():
             context += f"{k}: {v}\n"
         context += "\n"
-    answer = chain.invoke({"context": context, "question": question})
-    return answer
+    return context
 
+rag_chain = (
+    {"context": retriever | format_context, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+def generate_answer(question:str):
+    return rag_chain.invoke(question)
 
 if __name__ == "__main__":
     question = input("Enter a question (or 'q' to quit): ")
     while question != "q":
         answer = generate_answer(question)
         print(answer)
+        # print("-"*100)
+        # prompt_hub_rag = hub.pull("rlm/rag-prompt")
+        # print(prompt_hub_rag)
         question = input("Enter a question (or 'q' to quit): ")
 
 
