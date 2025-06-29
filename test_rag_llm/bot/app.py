87de5b118d9aca0ce075c2bf38aa79381aab494e
Hu Dou <hugh.dou@gmail.com>
2025-06-28 21:45:23 -0400

    test_rag_llm - improve web bot

--- test_rag_llm/bot/app.py
+++ test_rag_llm/bot/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import sys
 from pathlib import Path
+import re
 
 this_file = Path(__file__).resolve()
 rag_llm_dir = this_file.parents[1]
@@ -11,6 +12,15 @@ from generating import generate_answer
 
 app = Flask(__name__)
 
+# Custom Jinja filter to bold text between pairs of ** on the same line
+def bold_asterisks(value):
+    # Only replace pairs of ** on the same line
+    def replacer(match):
+        return f"<b>{match.group(1)}</b>"
+    return re.sub(r"\*\*(.+?)\*\*", replacer, value)
+
+app.jinja_env.filters['bold_asterisks'] = bold_asterisks
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     answer = None
@@ -19,6 +29,7 @@ def index():
         question = request.form.get('question', '')
         if question:
             answer = generate_answer(question)
+        print(answer)
     return render_template('index.html', answer=answer, question=question)
 
 if __name__ == '__main__':
